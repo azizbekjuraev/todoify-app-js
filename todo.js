61a1889f97de1b7c8e4b70f2f9ac38c75f825f6a
AZIZBEKJURAEV <ajuraev98@mail.ru>
2023-01-20 21:56:03 +0500

    added box-shadow, date, delete button

--- todo.js
+++ todo.js
@@ -6,24 +6,42 @@ const ul = document.querySelector(".todo--ul");
 
 let count = 1;
 
+//create date
+const weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+let d = new Date();
+let day = weekday[d.getDay()];
+
+const now = new Date();
+const options = {
+  hour: `numeric`,
+  minute: `numeric`,
+  day: `numeric`,
+  month: `numeric`,
+  year: `numeric`,
+  //weekday: `long`,
+};
+
+const today = new Intl.DateTimeFormat(options).format(now);
+
 addBtn.addEventListener("click", () => {
+  //created time;
+
   if (!inputEl.value) {
     return;
   }
-  if (inputEl.value === "") {
-  }
 
-  ul.innerHTML += `<h3 class="created">created: wed, 2023 </h3> <li>${count++}.  ${
+  ul.innerHTML += `<li class="li--text"> <h5 class="created">Created on: ${day} ${today} </h5>
+  ${count++}.${
     inputEl.value
   } <button class="delete--btn" >DELETE</button> </li>`;
   inputEl.value = "";
 
   // delete todo
-  let deleteBtn = document.querySelectorAll(".delete--btn");
+  const deleteBtn = document.querySelectorAll(".delete--btn");
+
   for (let i = 0; i < deleteBtn.length; i++) {
     deleteBtn[i].onclick = function () {
       this.parentNode.remove();
-
       if (ul.innerHTML === "") {
         count = 1;
       }
