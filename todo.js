49bde8612b16360ca8d243710f5fd8be3748075c
AZIZBEKJURAEV <ajuraev98@mail.ru>
2023-01-21 20:38:40 +0500

    added delete all btn, localStorage is not functional

--- todo.js
+++ todo.js
@@ -2,14 +2,16 @@
 
 const inputEl = document.getElementById("todo--input");
 const addBtn = document.getElementById("add--btn");
+const deleteAll = document.querySelector(".delete--all");
 const ul = document.querySelector(".todo--ul");
 
 let count = 1;
+let todo = [];
 
 //create date
 const weekday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 let d = new Date();
-let day = weekday[d.getDay()];
+let wDay = weekday[d.getDay()];
 
 const now = new Date();
 const options = {
@@ -22,29 +24,45 @@ const options = {
 };
 
 const today = new Intl.DateTimeFormat(options).format(now);
+const parsed = JSON.parse(localStorage.getItem("todos"));
+// console.log(parsed);
 
 addBtn.addEventListener("click", () => {
-  //created time;
-
   if (!inputEl.value) {
     return;
   }
 
-  ul.innerHTML += `<li class="li--text"> <h5 class="created">Created on: ${day} ${today} </h5>
-  ${count++}. ${
-    inputEl.value
-  } <button class="delete--btn" >DELETE</button> </li>`;
+  todo =
+    ul.innerHTML += `<li class="li--text"> <h5 class="created">Created on: ${wDay} ${today} </h5>
+${count++}. ${
+      inputEl.value
+    } <button class="delete--btn" >DELETE</button> </li>`;
   inputEl.value = "";
+
+  localStorage.setItem("todos", JSON.stringify(todo));
 
   // delete todo
   const deleteBtn = document.querySelectorAll(".delete--btn");
-
   for (let i = 0; i < deleteBtn.length; i++) {
     deleteBtn[i].onclick = function () {
       this.parentNode.remove();
       if (ul.innerHTML === "") {
         count = 1;
+        deleteAll.style.display = "none";
       }
+      //   setTimeout(() => {
+      //     alert(`Your todo was successfully deleted!`);
+      //   }, "2000");
     };
   }
+
+  //delete all todos
+  deleteAll.style.display = "inline-block";
+  deleteAll.addEventListener("click", () => {
+    ul.innerHTML = "";
+    if (ul.innerHTML === "") {
+      count = 1;
+      deleteAll.style.display = "none";
+    }
+  });
 });
